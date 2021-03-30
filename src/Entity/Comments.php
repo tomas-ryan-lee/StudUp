67b015d5a12fbd023b0c9184455167113cdf04d4
Umbardacil <kevin.myung.gaetan.jordan@eisti.fr>
2021-03-30 15:16:12 +0200

    Create classes without ORM integration

---
+++ src/Entity/Comments.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+class Comments {
+    private $id;
+    private $type;
+    private $author;
+    private $content;
+    private $publicationDateTime;
+    private $project;
+    private $isFromReferent;
+    private $projectRole;
+    private $collaborationProject;
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Entity;
+
+
+class User {
+    private $id;
+    private $login;
+    private $password;
+    private $school;
+    private $student;
+    private $referent;
+    private $lastConnection;
+}
