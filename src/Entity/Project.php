491d21451fea87e396b52bf2bf13b63c0f64aa92
Umbardacil <kevin.myung.gaetan.jordan@eisti.fr>
2021-04-19 20:02:02 +0200

    Create basic entity
    Creating basic entity without ORM integration and no advanced features.

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -4,4 +4,17 @@ namespace App\Entity;
 
 class Project {
     private $id;
+    private $name;
+    private $sector;
+    private $target;
+    private $phase;
+    private $incubator;
+    private $location;
+    private $document;
+    private $globalVision;
+    private $elevatorSpeech;
+    private $asset;
+    private $lookingFor;
+    private $mood;
+    private $members;
 }
