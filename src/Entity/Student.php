ae651d7a7fec322af27c99bcb16c6c7e57cc7dfa
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-05-20 13:09:29 +0200

    Integrate final BDD without ORM integration
    Integrate final BDD without ORM integration

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -4,22 +4,33 @@ namespace App\Entity;
 
 class Student {
     private $id;
+    # student or alumni
     private $status;
     private $surname;
     private $name;
+    private $gender;
     private $birthday;
     private $school;
+    private $studyLevel;
+    private $graduationYear;
     private $studentNumber;
+    private $studentCardPic;
     private $mail;
+    # link to user entity (one to one)
     private $user;
-    private $studentCard;
+    # link to job entity (many to many)
+    private $wantedJobs;
+    # link to domain entity (many to many)
     private $domains;
-    private $sector;
     private $newsFrequency;
+    private $profilePic;
+
+    # where is it ?
     private $website;
     private $linkedin;
     private $instagram;
     private $facebook;
-    private $profileImage;
+
+    # put false if user last connection > x month
     private $isActif;
 }
