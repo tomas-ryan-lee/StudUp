e9de682f81d8d49e1682f640799b199349a0c17b
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-05-20 14:58:41 +0200

    Add final ORM integration

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -1,9 +1,28 @@
 <?php
 
 namespace App\Entity;
+use Doctrine\ORM\Mappinga as ORM;
 
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="job")
+ */
 class Job{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="string")
+     */
     private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $category;
 }
