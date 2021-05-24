1a87f0671948b286cae951a2863074b73f36c2bd
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-05-24 15:02:46 +0200

    Job construction functions

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -8,11 +8,11 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity()
  * @ORM\Table(name="job")
  */
-class Job{
+class Job {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="integer")
      */
     private $id;
 
@@ -22,7 +22,16 @@ class Job{
     private $name;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=True)
      */
     private $category;
+
+
+    public function setName(string $name) {
+        $this->name = $name;
+    }
+
+    public function setCategory(string $category) {
+        $this->category = $category;
+    }
 }
