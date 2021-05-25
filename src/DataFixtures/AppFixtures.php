0c450866e14b2c9b51c9321fb9d6a4463b712882
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-05-26 01:20:08 +0200

    Add domain fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,48 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use App\Entity\Domain;
+
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        ## begin of domain fixtures
+        $domains = [
+            "Alimentaire",
+            "Audiovisuel",
+            "Bien-être & Sport",
+            "Cosmétique",
+            "Culture",
+            "Deeptech",
+            "Digital",
+            "Divertissement",
+            "Économie",
+            "Environnement",
+            "Évènementiel",
+            "Hôtellerie",
+            "Ingénieurie",
+            "Jeux vidéos",
+            "Luxe",
+            "Marketing & Comm",
+            "Mobilité",
+            "Mode",
+            "Musique",
+            "Restauration",
+            "Santé",
+            "Sciences humaines",
+            "Social",
+            "Technologie",
+            "Transport",
+            "Urbanisme"
+        ];
+        foreach($domains as $domainName) {
+            $domain = new Domain();
+            $domain->setName($domainName);
+            $manager->persist($domain);
+        };
+        ## end of domain fixtures
 
         $manager->flush();
     }
