99686bd889e1a288126a7f64c64322871efacdab
bbourdeleau <bbourdeleau@gmail.com>
2021-06-29 22:59:08 +0200

    Front end - first commit
    Favoris + page annuaire

---
+++ src/Controller/FavoritesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FavoritesController extends AbstractController
+{
+    /**
+     * @Route("/favorites", name="favorites")
+     */
+    public function index(): Response
+    {
+        return $this->render('favorites/index.html.twig', [
+            'controller_name' => 'FavoritesController',
+        ]);
+    }
+}
