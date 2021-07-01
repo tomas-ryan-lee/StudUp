23cf7496caf3b5724db8d8cc99dc71808d34a330
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-07-02 01:10:43 +0200

    Improvable but working connection form

--- src/Security/FormLoginAuthenticator.php
+++ src/Security/FormLoginAuthenticator.php
@@ -43,8 +43,9 @@ class FormLoginAuthenticator extends AbstractFormLoginAuthenticator
 
     public function supports(Request $request)
     {
-        return 'security_login' === $request->attributes->get('_route')
-            && $request->isMethod('POST');
+        return $request->isMethod('POST') &&
+            isset(json_decode($request->getContent(), true)["login"]) &&
+            isset(json_decode($request->getContent(), true)["password"]);
     }
 
     public function getCredentials(Request $request)
@@ -69,7 +70,7 @@ class FormLoginAuthenticator extends AbstractFormLoginAuthenticator
             throw new InvalidCsrfTokenException();
         }
 
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $credentials['username']]);
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['login' => $credentials['username']]);
 
         if (!$user) {
             // fail authentication with a custom error
@@ -95,6 +96,7 @@ class FormLoginAuthenticator extends AbstractFormLoginAuthenticator
 
     protected function getLoginUrl()
     {
+        
         return $this->router->generate('security_login');
     }
 }
