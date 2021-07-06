945b451051eb1fbea96c135b46aa0e6f30afdc17
jordankevi <jordan.kevin57@gmail.com>
2021-07-06 16:02:07 +0200

    Add project to controller
    Pass project info to the template

--- src/Controller/PageProjetController.php
+++ src/Controller/PageProjetController.php
@@ -6,15 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class PageProjetController extends AbstractController
 {
     /**
-     * @Route("/page/projet", name="page_projet")
+     * @Route("/page/projet/{id}", name="page_projet")
      */
-    public function index(): Response
+    public function index(int $id): Response
     {
+        $response = $this->forward('App\Controller\Api\ProjectController::get', ['id' => $id]);
+        $project = $response->data;
         return $this->render('page_projet/index.html.twig', [
             'controller_name' => 'PageProjetController',
+            'project' => $project,
         ]);
     }
 }
