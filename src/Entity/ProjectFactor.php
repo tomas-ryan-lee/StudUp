2c532c833a1b794e15d57ef25925a275dc8a7c3c
jordankevi <jordan.kevin57@gmail.com>
2021-07-07 11:07:17 +0200

    Create a class for storing algo factors
    That way, if the algorithm needs to evolve to be more accurate, only needs to modify this class

---
+++ src/Entity/ProjectFactor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+class ProjectFactor {
+    public const int $DOMAIN_FACTOR = 1;
+    public const int $LOCATION_FACTOR = 1;
+    public const int $CURRENT_PHASE_FACTOR = 1;
+    public const int $ASSET_FACTOR = 1;
+    public const int $LOOKING_FOR_FACTOR = 1;
+    public const int $MOOD_FACTOR = 1;
+    public const int $HAS_IMPACT_FACTOR = 1;
+    public const int $JOB_FACTOR = 1;
+    public const int $JOB_CATEGORY_FACTOR = 1;
+}
