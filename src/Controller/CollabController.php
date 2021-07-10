c5bb154a991121b0c04a7479f20f8d586152bad5
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-07-11 01:50:02 +0200

    Complete collab controller
    Give information to twig for template rendering
    Additionnaly change security bundle behavior to redirect to the home and
    open login modal instead of redirecting to login page when `IsGranted`
    is used for a controller

--- src/Controller/CollabController.php
+++ src/Controller/CollabController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,35 @@ class CollabController extends AbstractController
 {
     /**
      * @Route("/collab", name="collab")
+     * @IsGranted("ROLE_USER")
      */
     public function index(): Response
     {
+        $response = $this->forward('App\Controller\Api\ProjectMemberController::getAll');
+        $projectMembers = json_decode($response->getContent(), true);
+        $student = $this->getUser()->getProfile()->toArray();
+
+        $collaborations = [];
+        $applyments = [];
+        foreach($projectMembers as $projectMember) {
+            if($projectMember['student'] != null && $projectMember['student']['id'] == $student['id']) {
+                $collaborations[] = $projectMember;
+            } else {
+                $applicants = $projectMember['applicants'];
+                foreach ($applicants as $applicant) {
+                    if ($applicant['id'] == $student['id']) {
+                        $applyments[] = $projectMember;
+                    }
+                }
+            }
+        }
+
+
         return $this->render('collab/index.html.twig', [
             'controller_name' => 'CollabController',
+            'student' => $student,
+            'collaborations' => $collaborations,
+            'applyments' => $applyments
         ]);
     }
 }
