6c6ea246b9e75d87d3dd0be4f7a4ec45c6619a84
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-07-11 15:04:28 +0200

    Add 'ROLE_USER' for all authenticated users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -108,7 +108,9 @@ class User implements UserInterface {
     }
 
     public function getRoles() {
-        return $this->roles;
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
     }
 
     public function getLastConnection() {
