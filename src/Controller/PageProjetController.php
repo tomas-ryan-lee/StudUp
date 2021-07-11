6fc9b20b020e7a9e5804a1152bf251982c70f6f0
JORDAN Kévin <jordan.kevin57@gmail.com>
2021-07-11 22:51:10 +0200

    Add progress bar percentage for phases

--- src/Controller/PageProjetController.php
+++ src/Controller/PageProjetController.php
@@ -14,8 +14,20 @@ class PageProjetController extends AbstractController
      */
     public function index(int $id): Response
     {
+        $phases = [
+            "Étude de marché",
+            "Étude de besoins",
+            "Validation du concept",
+            "Rédaction du business plan",
+            "Création d’un MVP",
+            "Test utilisateurs",
+            "Lancement sur le marché"
+        ];
         $response = $this->forward('App\Controller\Api\ProjectController::get', ['id' => $id]);
         $project = json_decode($response->getContent(), true);
+
+        $percentage = (100 * (array_search($project['currentPhase'], $phases) + 1) / 8) + "%";
+
         if (isset($project['video'])) {
             $project['video'] = str_replace('watch?v=', 'embed/', $project['video']);
         }
@@ -24,6 +36,7 @@ class PageProjetController extends AbstractController
             [
                 'controller_name' => 'PageProjetController',
                 'project' => $project,
+                'percentage' => $percentage,
             ]
         );
     }
